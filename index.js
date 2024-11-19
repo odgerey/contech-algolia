bf5c2de0f373fdd9932efdfea35ff78dc4bd0047
Audrey Medaino-Tardif <audreymedaino-tardif@Audreys-MacBook-Air.local>
2024-11-18 20:40:34 -0500

    Reset

---
+++ index.js
@@ -0,0 +1,22 @@
+import { liteClient as algoliasearch } from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import { searchBox, hits } from 'instantsearch.js/es/widgets';
+
+const searchClient = algoliasearch('RTGNQ6F69C', 'd00c63705fb78ab7047ffe39ff3ce47b');
+
+const search = instantsearch({
+  indexName: 'demo_ecommerce',
+  searchClient,
+});
+
+search.addWidgets([
+  searchBox({
+    container: "#searchbox"
+  }),
+
+  hits({
+    container: "#hits"
+  })
+]);
+
+search.start();
